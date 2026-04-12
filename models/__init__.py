5c9ae19daf289c73f8d5aa6618387b695b5e593c
gaosh <gao.shanqian@gmail.com>
2026-04-12 14:43:46 -0400

    updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
 from .tokenizer import LlamaTokenizer
 from .modeling_llama_pruning import LlamaForCausalLM as PruneLlamaForCausalLM
 from .modeling_llama_pruning import LlamaDecoderLayer as PruneLlamaDecoderLayer
+
+from .modeling_qwen3_pruning import (
+    Qwen3ForCausalLM as PruneQwen3ForCausalLM,
+)
+from .modeling_qwen3_pruning import (
+    Qwen3DecoderLayer as PruneQwen3DecoderLayer,
+)
